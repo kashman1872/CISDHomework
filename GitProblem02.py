7188015081edf5a972e7d3ecf957532090dd5e13
Kristyna Ashman <kmathews-ashman@student.waubonsee.edu>
2020-04-07 20:24:43 -0500

    Final Commit

--- GitProblem02.py
+++ GitProblem02.py
@@ -4,5 +4,7 @@ num2 = int(input('Enter second number: '))
 
 if num1 > num2:
     print('Number 1 is bigger than Number 2')
+elif num1 < num2:
+    print('Number 2 is bigger than Number 1')
 else:
     print('Number 1 is equal to Number 2')
