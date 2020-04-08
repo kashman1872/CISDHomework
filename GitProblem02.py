10606f0209c1f284e5067bffdc9df8d2b4bca08c
Kristyna Ashman <kmathews-ashman@student.waubonsee.edu>
2020-04-07 19:39:48 -0500

    Initial GitProblem02 Commit

---
+++ GitProblem02.py
@@ -0,0 +1,8 @@
+# Ask user for two numbers and compare. Display message when 1st number is greater
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+
+if num1 > num2:
+    print('num 1 is bigger than num 2')
+else:
+    print('')
